196903cef60dd20b21a197c5ea86645d38ba39bf
chun <lamchunyu00@gmail.com>
2021-12-23 14:12:32 +0800

    Created main page

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,32 +1,60 @@
 import React from 'react'
-import './styles/index.styled'
-import {MainContainer, MainContent, Links, Navigation, Arrow, TermText} from "./styles/index.styled";
+import {
+  MainContainer,
+  MainContent,
+  Links,
+  Navigation,
+  Arrow,
+  LeftArrow,
+  TermText,
+  SquarePicture,
+  NavContainer,
+  SettingsContainer,
+  Settings,
+  CenterContainer,
+} from "./styles/index.styled";
+import ProfilePicture from '../assets/images/profile.jpg'
+
+//TODO:
+// - Main Content needs to be mobile friendly
+// - Hide current nav bar and replace with mobile navbar
+// - Add bottom navbar
+// - Add some blur
 
 export const Home = () => {
   return <MainContainer>
-    <Navigation>
-      <Links col='#bd93f9'> ~/chunyu</Links><Arrow col='#bd93f9' />
-      <Links col='#f1fa8c'> about</Links><Arrow col='#f1fa8c' />
-      <Links col='#8be9fd'>華 experience</Links><Arrow col='#8be9fd' />
-      <Links col='#ffb86c'> projects</Links><Arrow col='#ffb86c' />
-      <TermText>neofetch</TermText>
-    </Navigation>
-    <MainContent>
-      <br />
-      chunyu@nus<br />
-      ------------<br />
-      OS: Windows/Arch Linux<br />
-      Name: Chun Yu<br />
-      Uptime: 21 Years<br />
-      Resolution: 1920x1080<br />
-      WM: Explorer/AwesomeWM<br /><br />
-       : Full-Time Student @ NUS<br />
-       : Computer Science<br />
-      : Windows Terminal/kitty<br />
-      <br />
-      {/*TODO: Color scheme?*/}
-      {/*colors here<br />*/}
-       linkedin  github  email
-    </MainContent>
+    <CenterContainer>
+      <NavContainer>
+        <Navigation>
+          <Links col='#bd93f9'> ~/chunyu</Links><Arrow col='#bd93f9'/>
+          <Links col='#f1fa8c'> /about</Links><Arrow col='#f1fa8c'/>
+          <Links col='#8be9fd'>華 /experience</Links><Arrow col='#8be9fd'/>
+          <Links col='#ffb86c'> /projects</Links><Arrow col='#ffb86c'/>
+          <TermText>neofetch</TermText>
+        </Navigation>
+        <SettingsContainer>
+          <LeftArrow col='#ffb86c' /><Settings col='#ffb86c'>Sat 10 Dec 16:43</Settings>
+        </SettingsContainer>
+      </NavContainer>
+      <MainContent>
+        <SquarePicture src={ProfilePicture}/>
+        <div>
+          chunyu@nus<br/>
+          ------------<br/>
+          OS: Windows/Arch Linux<br/>
+          Name: Chun Yu<br/>
+          Uptime: 21 Years<br/>
+          Resolution: 1920x1080<br/>
+          WM: Explorer/AwesomeWM<br/><br/>
+           : Full-Time Student @ NUS<br/>
+           : Computer Science<br/>
+          : Windows Terminal/kitty<br/>
+          <br/>
+          {/*TODO: Color scheme?*/}
+          {/*colors here<br />*/}
+           linkedin  github  email
+        </div>
+      </MainContent>
+    </CenterContainer>
   </MainContainer>
 }
--- src/home/styles/index.styled.ts
+++ src/home/styles/index.styled.ts
@@ -3,19 +3,48 @@ import * as color from "../../color";
 
 export const MainContainer = styled.div`
   background-color: ${color.background};
-  min-height: 100vh;
-  //font-size: calc(10px + 2vmin);
   color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  max-height: 100vh;
+  max-width: 100vw;
+  overflow: hidden;
+`
+
+export const CenterContainer = styled.div`
+  width: calc(100% - 40px);
+  height: calc(100% - 40px);
+  border: 1px solid white;
+  border-radius: 20px;
+`
+
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px; 
+  align-items: center;
+`
+
+export const SettingsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 `
 
 export const MainContent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  //height: calc(100vh - 65px);
+  height: 80%;
 `
 
 export const Navigation = styled.div`
-  width: 100%;
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -24,22 +53,45 @@ export const Navigation = styled.div`
 export const Arrow = styled.div<{ col: string }>`
   width: 0;
   height: 0;
-  border-top: 10px solid transparent;
-  border-bottom: 10px solid transparent;
-  border-left: 10px solid ${(props) => props.col ? props.col : color.orange};
+  border-top: 12.5px solid transparent;
+  border-bottom: 12.5px solid transparent;
+  border-left: 12.5px solid ${(props) => props.col ? props.col : color.orange};
   z-index: 1;
-  margin-right: -10px;
+  margin-right: -12.5px;
+  cursor: pointer;
+`
+
+export const LeftArrow = styled.div<{ col: string }>`
+  width: 0;
+  height: 0;
+  border-top: 12.5px solid transparent;
+  border-bottom: 12.5px solid transparent;
+  border-right: 12.5px solid ${(props) => props.col ? props.col : color.orange};
+  z-index: 1;
   cursor: pointer;
 `
 
 export const TermText = styled.div`
-  padding: 0 5px 0 15px;
+  padding: 0 5px 0 20px;
 `
 
-//TODO: Adapt this to changable themes
+export const SquarePicture = styled.img`
+  width: 250px;
+  height: 250px;
+  margin: 50px;
+`
+
 export const Links = styled.div<{ col: string }>`
   background: ${(props) => props.col ? props.col : color.orange};
-  padding: 0 5px 0 15px;
+  padding: 0 5px 0 25px;
+  color: black;
+  cursor: pointer;
+  white-space: nowrap;
+`
+
+export const Settings = styled.div<{ col: string }>`
+  background: ${(props) => props.col ? props.col : color.orange};
+  padding: 0 20px 0 0;
   color: black;
   cursor: pointer;  
 `
